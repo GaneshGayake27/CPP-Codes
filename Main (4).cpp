8a46ac130e1f066848a298cc6135d4aeeed4fdb2
GaneshGayake27 <ganesh.gayake24@sanjivani.edu.in>
2024-08-26 09:47:30 +0530

    Add files via upload

---
+++ Main (4).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=6,b=7;
+    
+    cout<<"a&bis"<<(a&b)<<endl;
+    cout<<"a|bis"<<(a|b)<<endl;
+    cout<<"a^bis"<<(a^b)<<endl;
+    cout<<"a<<1is"<<(a<<1)<<endl;
+    cout<<"a>>1is"<<(a>>1)<<endl;
+    cout<<"~(a)is"<<(~a)<<endl;
+
+    return 0;
+}
